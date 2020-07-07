8f48f827a5c5b4fe376b0e0202f0b896d1fa0774
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-07-07 23:12:36 +0300

    Updates

--- burger.d00f4dfd.js
+++ burger.d00f4dfd.js
@@ -117,83 +117,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
-
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
-  }
-
-  return bundleURL;
-}
-
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
-
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
-  }
-
-  return '/';
-}
-
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
-}
-
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
-
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
-  };
-
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
-  }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
-}
-
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"sass/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-mobile3.jpg":[["hero-mobile3.4bb19e28.jpg","images/hero/hero-mobile3.jpg"],"images/hero/hero-mobile3.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-mobile3@2x.jpg":[["hero-mobile3@2x.020b9fc0.jpg","images/hero/hero-mobile3@2x.jpg"],"images/hero/hero-mobile3@2x.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-mobile3@3x.jpg":[["hero-mobile3@3x.e7063531.jpg","images/hero/hero-mobile3@3x.jpg"],"images/hero/hero-mobile3@3x.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-tablet3.jpg":[["hero-tablet3.1b8b5c8d.jpg","images/hero/hero-tablet3.jpg"],"images/hero/hero-tablet3.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-tablet3@2x.jpg":[["hero-tablet3@2x.b4536b8e.jpg","images/hero/hero-tablet3@2x.jpg"],"images/hero/hero-tablet3@2x.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-tablet3@3x.jpg":[["hero-tablet3@3x.25bf69e4.jpg","images/hero/hero-tablet3@3x.jpg"],"images/hero/hero-tablet3@3x.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-desctop3.jpg":[["hero-desctop3.95eaaa6c.jpg","images/hero/hero-desctop3.jpg"],"images/hero/hero-desctop3.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-desctop3@2x.jpg":[["hero-desctop3@2x.93f91e41.jpg","images/hero/hero-desctop3@2x.jpg"],"images/hero/hero-desctop3@2x.jpg"],"E:\\Projects\\github-projects\\beard-barbershop\\src\\images\\hero\\hero-desctop3@3x.jpg":[["hero-desctop3@3x.c9e876f3.jpg","images/hero/hero-desctop3@3x.jpg"],"images/hero/hero-desctop3@3x.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./sass/main.scss");
-},{"./sass/main.scss":"sass/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"burger.js":[function(require,module,exports) {
+(function () {
+  var menuBtnRef = document.querySelector('[data-menu-button]');
+  var mobileMenuRef = document.querySelector('[data-menu]');
+  menuBtnRef.addEventListener('click', function () {
+    var expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -397,5 +332,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","burger.js"], null)
+//# sourceMappingURL=/burger.d00f4dfd.js.map
